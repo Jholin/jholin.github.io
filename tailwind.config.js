f3f002206dbcdc0d62bc6d9a98785733d6dcf4a1
Jholin Ibrahimovic <jholin04@gmail.com>
2023-12-16 23:35:38 +0700

    deleted tailwindcss file

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./contact.html", "./portofolio.html", "./contact.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
